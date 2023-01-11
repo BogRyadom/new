ba4bb4ce155f1f5cdd8020dbf190697c2959ef88
BogRyadom <divine.bohu@gmail.com>
2023-01-12 01:56:14 +0200

    add method 'stepen'

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿ static int Stepen(int a, int b)
+        {
+            int c = a;
+            int d = b;
+
+            int per = a;
+
+            for (int i = 1; i < b; i++)
+            {
+                a *= per;
+            }
+            return a;
+        }
